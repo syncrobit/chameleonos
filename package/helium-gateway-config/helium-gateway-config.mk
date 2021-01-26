25b6f8d4a5c2f19858a10f67e78ffdd9a4b8b4c8
Calin Crisan <ccrisan@gmail.com>
2021-01-26 23:59:08 +0200

    Add helium-{gateway-config,packet-forwarder} packages

--- package/helium-gateway-config/helium-gateway-config.mk
+++ package/helium-gateway-config/helium-gateway-config.mk
@@ -1,18 +1,18 @@
 ################################################################################
 #
-# helium-miner
+# helium-gateway-config
 #
 ################################################################################
 
-HELIUM_MINER_VERSION = 2021.01.22.0
-HELIUM_MINER_SITE = $(call github,helium,miner,$(HELIUM_MINER_VERSION))
-HELIUM_MINER_LICENSE = Apache-2.0
-HELIUM_MINER_LICENSE_FILES = LICENSE
-HELIUM_MINER_DEPENDENCIES = dbus gmp libsodium erlang host-rust-bin host-erlang-rebar
+HELIUM_GATEWAY_CONFIG_VERSION = 2021.01.22.0
+HELIUM_GATEWAY_CONFIG_SITE = $(call github,helium,gateway-config,$(HELIUM_GATEWAY_CONFIG_VERSION))
+HELIUM_GATEWAY_CONFIG_LICENSE = Apache-2.0
+HELIUM_GATEWAY_CONFIG_LICENSE_FILES = LICENSE
+HELIUM_GATEWAY_CONFIG_DEPENDENCIES = dbus gmp libsodium erlang host-rust-bin host-erlang-rebar
             
-HELIUM_MINER_POST_EXTRACT_HOOKS += HELIUM_MINER_FETCH_PATCH_DEPS
+#HELIUM_GATEWAY_CONFIG_POST_EXTRACT_HOOKS += HELIUM_GATEWAY_CONFIG_FETCH_PATCH_DEPS
 
-define HELIUM_MINER_FETCH_PATCH_DEPS
+define HELIUM_GATEWAY_CONFIG_FETCH_PATCH_DEPS
     (cd $(@D); \
             CC="$(TARGET_CC)" \
             CXX="$(TARGET_CXX)" \
@@ -35,37 +35,29 @@ define HELIUM_MINER_FETCH_PATCH_DEPS
     patch -d $(@D)/_build/default/lib/rocksdb -p1 < package/helium-miner/erlang-rocksdb._patch
 endef
             
-define HELIUM_MINER_BUILD_CMDS
+define HELIUM_GATEWAY_CONFIG_BUILD_CMDS
     (cd $(@D); \
             CC="$(TARGET_CC)" \
             CXX="$(TARGET_CXX)" \
             CFLAGS="$(TARGET_CFLAGS) -U__sun__" \
             CXXFLAGS="$(TARGET_CXXFLAGS)" \
             LDFLAGS="$(TARGET_LDFLAGS) -L $(STAGING_DIR)/usr/lib/erlang/lib/erl_interface-$(ERLANG_EI_VSN)/lib" \
-            ERLANG_ROCKSDB_OPTS="-DWITH_BUNDLE_SNAPPY=ON -DWITH_BUNDLE_LZ4=ON" \
-            ERL_COMPILER_OPTIONS="[deterministic]" \
-            ERTS_INCLUDE_DIR="$(STAGING_DIR)/usr/lib/erlang/erts-10.6/include" \
             $(REBAR_TARGET_DEPS_ENV) \
             $(TARGET_MAKE_ENV) \
-            CARGO_HOME=$(HOST_DIR)/share/cargo \
-            CARGO_BUILD_TARGET=aarch64-unknown-linux-gnu \
-            ./rebar3 as prod tar -n miner \
+            ./rebar3 as prod tar -n gateway_config \
     )
 endef
 
-define HELIUM_MINER_INSTALL_TARGET_CMDS
-    mkdir -p $(TARGET_DIR)/opt/miner; \
-    cd $(TARGET_DIR)/opt/miner; \
-    tar -zxvf $(@D)/_build/prod/rel/*/*.tar.gz; \
-    mkdir -p update; \
-    wget https://github.com/helium/blockchain-api/raw/master/priv/prod/genesis -O update/genesis; \
-    cp $(TARGET_DIR)/usr/lib/erlang/bin/no_dot_erlang.boot .
+define HELIUM_GATEWAY_CONFIG_INSTALL_TARGET_CMDS
+    mkdir -p $(TARGET_DIR)/opt/gateway_config; \
+    cd $(TARGET_DIR)/opt/gateway_config; \
+    tar -zxvf $(@D)/_build/prod/rel/*/*.tar.gz;
 endef
 
-define HELIUM_MINER_INSTALL_STAGING_CMDS
+define HELIUM_GATEWAY_CONFIG_INSTALL_STAGING_CMDS
 endef
 
-define HELIUM_MINER_INSTALL_CMDS
+define HELIUM_GATEWAY_CONFIG_INSTALL_CMDS
 endef
 
 $(eval $(generic-package))
---
+++ package/helium-packet-forwarder/helium-packet-forwarder.mk
@@ -0,0 +1,34 @@
+################################################################################
+#
+# helium-packet-forwarder
+#
+################################################################################
+
+HELIUM_PACKET_FORWARDER_VERSION = 2021.01.22.0
+HELIUM_PACKET_FORWARDER_SITE = $(call github,helium,sx1302_hal,$(HELIUM_PACKET_FORWARDER_VERSION))
+HELIUM_PACKET_FORWARDER_LICENSE = Apache-2.0
+HELIUM_PACKET_FORWARDER_LICENSE_FILES = LICENSE
+            
+define HELIUM_PACKET_FORWARDER_BUILD_CMDS
+    (cd $(@D); \
+            CROSS_COMPILE=aarch64-none-linux-gnu- \
+            $(TARGET_MAKE_ENV) \
+            make \
+    )
+endef
+
+define HELIUM_PACKET_FORWARDER_INSTALL_TARGET_CMDS
+    mkdir -p $(TARGET_DIR)/packet_forwarder/bin
+    mkdir -p $(TARGET_DIR)/packet_forwarder/etc
+    cp $(@D)/tools/reset_lgw.sh $(TARGET_DIR)/packet_forwarder/bin
+    cp $(@D)/packet_forwarder/lora_pkt_fwd $(TARGET_DIR)/packet_forwarder/bin
+    cp package/helium-packet-forwarder/global_conf_sx1302.json $(TARGET_DIR)/packet_forwarder/etc
+endef
+
+define HELIUM_PACKET_FORWARDER_INSTALL_STAGING_CMDS
+endef
+
+define HELIUM_PACKET_FORWARDER_INSTALL_CMDS
+endef
+
+$(eval $(generic-package))
