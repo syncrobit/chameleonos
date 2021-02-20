f4f7239bc036347690a921dee715283c4b8143a8
Calin Crisan <ccrisan@gmail.com>
2021-02-20 22:26:01 +0200

    Rename helium-packet-forwarder to helium-packet-forwarder-sx1302

---
+++ package/helium-packet-forwarder-sx1302/helium-packet-forwarder-sx1302.mk
@@ -0,0 +1,39 @@
+################################################################################
+#
+# helium-packet-forwarder-sx1302
+#
+################################################################################
+
+HELIUM_PACKET_FORWARDER_SX1302_VERSION = 2021.02.12.0
+HELIUM_PACKET_FORWARDER_SX1302_SITE = $(call github,helium,sx1302_hal,$(HELIUM_PACKET_FORWARDER_SX1302_VERSION))
+HELIUM_PACKET_FORWARDER_SX1302_LICENSE = Apache-2.0
+HELIUM_PACKET_FORWARDER_SX1302_LICENSE_FILES = LICENSE
+            
+define HELIUM_PACKET_FORWARDER_SX1302_BUILD_CMDS
+    (cd $(@D); \
+            CROSS_COMPILE=aarch64-none-linux-gnu- \
+            $(TARGET_MAKE_ENV) \
+            make \
+    )
+endef
+
+define HELIUM_PACKET_FORWARDER_SX1302_INSTALL_TARGET_CMDS
+    mkdir -p $(TARGET_DIR)/opt/packet_forwarder/bin
+    mkdir -p $(TARGET_DIR)/opt/packet_forwarder/etc
+    ln -sf /usr/bin/reset_lgw.sh $(TARGET_DIR)/opt/packet_forwarder/bin
+    cp $(@D)/packet_forwarder/lora_pkt_fwd $(TARGET_DIR)/opt/packet_forwarder/bin/lora_pkt_fwd_sx1302
+    cp $(@D)/util_chip_id/chip_id $(TARGET_DIR)/opt/packet_forwarder/bin
+    cp $(@D)/util_net_downlink/net_downlink $(TARGET_DIR)/opt/packet_forwarder/bin
+    for file in $(@D)/packet_forwarder/global_conf.json.sx1250.*; do \
+        fname=$$(basename "$${file}" | sed 's/sx1250/sx1302/'); \
+        cp "$${file}" $(TARGET_DIR)/opt/packet_forwarder/etc/$${fname}; \
+    done
+endef
+
+define HELIUM_PACKET_FORWARDER_SX1302_INSTALL_STAGING_CMDS
+endef
+
+define HELIUM_PACKET_FORWARDER_SX1302_INSTALL_CMDS
+endef
+
+$(eval $(generic-package))
