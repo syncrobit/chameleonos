21feb07225642f10321facf2a640e451d979d043
Calin Crisan <ccrisan@gmail.com>
2021-03-04 20:35:14 +0200

    Add support for Raspberry Pi 3

--- board/raspberrypi4arm64/postscript.sh
+++ board/raspberrypi4arm64/postscript.sh
@@ -10,8 +10,11 @@ cp ${BOARD_DIR}/initrd.gz ${BOOT_DIR}
 gzip -c ${IMG_DIR}/Image > ${BOOT_DIR}/kernel8.img
 cp ${IMG_DIR}/bcm2711-rpi-4-b.dtb ${BOOT_DIR}
 cp ${IMG_DIR}/bcm2711-rpi-cm4.dtb ${BOOT_DIR}
+cp ${IMG_DIR}/bcm2710-rpi-3-b.dtb ${BOOT_DIR}
+cp ${IMG_DIR}/bcm2710-rpi-3-b-plus.dtb ${BOOT_DIR}
 cp ${RPI_FW_DIR}/start.elf ${BOOT_DIR}
 cp ${RPI_FW_DIR}/fixup.dat ${BOOT_DIR}
+cp ${RPI_FW_DIR}/bootcode.bin ${BOOT_DIR}
 
 # copy overlays
 mkdir -p ${BOOT_DIR}/overlays
