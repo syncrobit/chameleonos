cbff09c95ced91a27d1d0bd8587386cb9203dceb
Calin Crisan <ccrisan@gmail.com>
2021-03-26 00:01:12 +0200

    miner: Update to 2021.03.24.0

--- package/helium-miner/helium-miner.mk
+++ package/helium-miner/helium-miner.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HELIUM_MINER_VERSION = 2021.03.22.0
+HELIUM_MINER_VERSION = 2021.03.24.0
 HELIUM_MINER_SITE = $(call github,helium,miner,$(HELIUM_MINER_VERSION))
 HELIUM_MINER_LICENSE = Apache-2.0
 HELIUM_MINER_LICENSE_FILES = LICENSE
@@ -29,8 +29,6 @@ define HELIUM_MINER_FETCH_PATCH_DEPS
             ./rebar3 get-deps \
     )
     
-    patch -d $(@D)/_build/default/lib/erasure -p1 < package/helium-miner/erlang-erasure._patch
-    patch -d $(@D)/_build/default/lib/erlang_pbc -p1 < package/helium-miner/erlang-pbc._patch
     patch -d $(@D)/_build/default/lib/procket -p1 < package/helium-miner/procket._patch
 endef
             
