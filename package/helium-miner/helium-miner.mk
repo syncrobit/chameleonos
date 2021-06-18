c021ebabf2f7b2db730cfae8ecd122f3620d471f
Calin Crisan <ccrisan@gmail.com>
2021-06-18 23:26:10 +0300

    miner: Update to 2021.06.18.0

--- package/helium-miner/helium-miner.mk
+++ package/helium-miner/helium-miner.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HELIUM_MINER_VERSION = 2021.06.14.0
+HELIUM_MINER_VERSION = 2021.06.18.0
 HELIUM_MINER_SITE = $(call github,helium,miner,$(HELIUM_MINER_VERSION))
 HELIUM_MINER_LICENSE = Apache-2.0
 HELIUM_MINER_LICENSE_FILES = LICENSE
