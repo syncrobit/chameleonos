e4054d8794911f6e82ca79a4e80ccf3312295a45
Calin Crisan <ccrisan@gmail.com>
2021-08-05 23:27:08 +0300

    hotspot-api-server: Add internal (dashboard) user support

--- board/common/overlay/opt/hotspot-api-server/user.py
+++ board/common/overlay/opt/hotspot-api-server/user.py
@@ -1,14 +1,34 @@
 
+import hashlib
 import logging
 import os
+import pubkey
 
 
 CREDENTIALS_FILE = '/var/lib/api_credentials'
 DEFAULT_USERNAME = 'admin'
 DEFAULT_PASSWORD = 'admin'
+INTERNAL_USERNAME = 'dashboard'
+
+
+def verify_credentials_internal(username: str, password: str) -> bool:
+    if username != INTERNAL_USERNAME:
+        return False
+
+    actual_password = f'{pubkey.get_pub_key_hex()}:{pubkey.get_ecc_sn()}'
+    actual_password = hashlib.sha256(actual_password.encode()).hexdigest()
+
+    if password != actual_password:
+        logging.error('internal password mismatch')
+        return False
+
+    return True
 
 
 def verify_credentials(username: str, password: str) -> bool:
+    if verify_credentials_internal(username, password):
+        return True
+
     if os.path.exists(CREDENTIALS_FILE) and os.path.getsize(CREDENTIALS_FILE) > 0:
         with open(CREDENTIALS_FILE, 'rt') as f:
             actual_username, actual_password = f.readline().strip().split(':', 1)
