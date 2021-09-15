0e42a8c42a66b4af68bfc2337a46861e28693ca2
Calin Crisan <ccrisan@gmail.com>
2021-09-15 19:52:49 +0300

    Add build and deploy helper scripts

---
+++ build-all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd $(dirname $0)
+set -e
+set -a
+
+if [[ -z "${VERSION}" ]]; then
+    echo "VERSION variable is unset"
+    exit 1
+fi
+
+VENDORS="syncrobit linxdot"
+
+export THINGOS_VERSION=${VERSION}
+
+for VENDOR in ${VENDORS}; do
+    echo "Building for ${VENDOR}"
+    ./build.sh raspberrypi4arm64
+    ./build.sh raspberrypi4arm64 mkimage
+    ./build.sh raspberrypi4arm64 mkrelease
+done
---
+++ deploy-all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd $(dirname $0)
+set -e
+set -a
+
+if [[ -z "${VERSION}" ]]; then
+    echo "VERSION variable is unset"
+    exit 1
+fi
+
+if [[ -z "$1" ]]; then
+    echo "Missing first argument"
+    exit 1
+fi
+
+VENDORS="syncrobit linxdot"
+
+set -a
+for VENDOR in ${VENDORS}; do
+    echo "Deploying for ${VENDOR}"
+    source vendors/${VENDOR}.conf
+    ./deploy.sh $1 output/raspberrypi4arm64/images/chameleonos-${THINGOS_PREFIX}-raspberrypi4arm64-${VERSION}.img.xz
+done
