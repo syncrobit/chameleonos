21ffce4e773ef9ee21ddc67aa354565805d99c49
Calin Crisan <ccrisan@gmail.com>
2021-12-05 22:52:14 +0200

    Update python-pyatecc to 5064a387fd38943baa333511d28c5800a6ca590f

--- package/python-pyatecc/python-pyatecc.mk
+++ package/python-pyatecc/python-pyatecc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PYATECC_VERSION = d62468d70f986bd8dcff2b93f063c19201ee5483
+PYTHON_PYATECC_VERSION = 5064a387fd38943baa333511d28c5800a6ca590f
 PYTHON_PYATECC_SITE = $(call github,ccrisan,pyatecc,$(PYTHON_PYATECC_VERSION))
 PYTHON_PYATECC_SETUP_TYPE = setuptools
 PYTHON_PYATECC_LICENSE = MIT
