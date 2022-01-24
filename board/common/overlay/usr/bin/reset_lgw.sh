610dee8f18102ad4301d371c6ca8ffcbccffffbd
Calin Crisan <ccrisan@gmail.com>
2022-01-24 20:19:34 +0200

    Use raspi-gpio for driving reset GPIO(s)

--- board/common/overlay/usr/bin/reset_lgw.sh
+++ board/common/overlay/usr/bin/reset_lgw.sh
@@ -5,10 +5,10 @@ source /var/run/hardware.conf
 IFS=, GPIOS=(${LGW_RESET_GPIO}); unset IFS
 
 for gpio in ${GPIOS[@]}; do
-    gpio.sh ${gpio} 0
+    raspi-gpio set ${gpio} op dl
     usleep 100000
-    gpio.sh ${gpio} 1
+    raspi-gpio set ${gpio} op dh
     usleep 100000
-    gpio.sh ${gpio} 0
+    raspi-gpio set ${gpio} op dl
     usleep 100000
 done
