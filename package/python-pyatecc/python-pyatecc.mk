3585ff374c72b4700e2ba5bf64108a5f8e4383ce
Calin Crisan <ccrisan@gmail.com>
2022-02-26 20:18:49 +0200

    python-pyatecc: Fix compatibility issue

--- package/python-pyatecc/python-pyatecc.mk
+++ package/python-pyatecc/python-pyatecc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PYATECC_VERSION = 5064a387fd38943baa333511d28c5800a6ca590f
+PYTHON_PYATECC_VERSION = 399634c122835d894a3f2fdfa645830c736f3d38
 PYTHON_PYATECC_SITE = $(call github,ccrisan,pyatecc,$(PYTHON_PYATECC_VERSION))
 PYTHON_PYATECC_SETUP_TYPE = setuptools
 PYTHON_PYATECC_LICENSE = MIT
