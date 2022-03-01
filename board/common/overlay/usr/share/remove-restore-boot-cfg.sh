e8207dcceb8de0a123c8ed0ff6a0ae38cdfffb9f
Calin Crisan <ccrisan@gmail.com>
2022-03-01 19:53:52 +0200

    Restore external antenna config on firmware upgrade

--- board/common/overlay/usr/share/remove-restore-boot-cfg.sh
+++ board/common/overlay/usr/share/remove-restore-boot-cfg.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 mount -o remount,rw /
-if [[ -f /usr/libexec/fw-restore-boot-cfg ]]; then
-    rm /usr/libexec/fw-restore-boot-cfg
-fi
+echo -e '#!/bin/bash\ngrep -q dtparam=ant2 $1/config.txt && echo dtparam=ant2 >> /boot/config.txt' > /usr/libexec/fw-restore-boot-cfg
+chmod +x /usr/libexec/fw-restore-boot-cfg
